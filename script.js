c167e5f2da93af7fc3f662e9df39eedddde11134
IderaNimah <iderayusuf29@gmail.com>
2023-08-31 01:46:50 +0100

    email

--- script.js
+++ script.js
@@ -3,8 +3,16 @@ const submitButton = document.getElementById("submit");
 const mainContainer = document.querySelector(".main_container");
 console.log("thanksConfirmation");
 
-
 submitButton.addEventListener("click", () => {
-    thanksConfirmation.classList.remove("hidden")
-    mainContainer.style.display="none"
-  })
+  thanksConfirmation.classList.remove("hidden");
+  mainContainer.style.display = "none";
+});
+
+document.getElementById("submit").addEventListener("click", function () {
+  const email = document.getElementById("email").value;
+  const emailSpan = document.getElementById("emailSpan");
+  emailSpan.textContent = email;
+
+  // Show the confirmation section
+  document.querySelector(".thank_you").classList.remove("hidden");
+});
